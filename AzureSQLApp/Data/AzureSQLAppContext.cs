dbe35d54151d7c3bd44f219dbfa0b6cbc04393dc
Hariom Sharma <hariom.sharma@globant.com>
2024-10-23 19:44:47 +0530

    Add project files.

---
+++ AzureSQLApp/Data/AzureSQLAppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AzureSQLApp.Models;
+
+namespace AzureSQLApp.Data
+{
+    public class AzureSQLAppContext : DbContext
+    {
+        public AzureSQLAppContext (DbContextOptions<AzureSQLAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AzureSQLApp.Models.Employee> Employees { get; set; } = default!;
+        public DbSet<AzureSQLApp.Models.Department> Departments { get; set; } = default!;
+    }
+}
---
+++ AzureSQLApp/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AzureSQLApp.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Department")]
+        public int FKDeptId { get; set; }
+        public string EmpName { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+        public Department Department { get; set; }
+
+    }
+}
